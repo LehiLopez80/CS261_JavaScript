0c306695cfe8461b092fea9137e15b9c7621a3c0
LehiLopez80 <39324745+LehiLopez80@users.noreply.github.com>
2019-05-18 22:18:26 -0500

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,14 @@
+var xhttp = new XMLHttpRequest();
+
+xhttp.open("GET", "ajax_info.txt", true);
+xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhttp.send();
+
+xhttp.onreadystatechange = function() {
+   if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+   }
+};
+
+xhttp.open("GET", "ajax_info.txt", true);
+xhttp.send();
